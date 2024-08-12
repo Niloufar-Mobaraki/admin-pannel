e3e3da2caf41eb40c2f80fa07c9510b8097f9d72
Niloufar-Mobaraki <mobaraki.niloofar@gmail.com>
2024-08-12 16:06:30 +0330

    footer add

---
+++ Desktop/New folder (2)/Js/script.js
@@ -0,0 +1,11 @@
+var number1 = prompt("لطفا عدد اول را وارد نمایید :")
+var number2 = prompt("لطفا عدد دوم را وارد نمایید :")
+
+
+var number1number = +number1
+var number2number = +number2
+
+
+var plusresult = number1number + number2number
+
+alert (plusresult) 
